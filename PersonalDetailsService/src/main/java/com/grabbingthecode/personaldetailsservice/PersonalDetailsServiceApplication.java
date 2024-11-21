61fe25b5161ee30e04d279246a7a961a0d794be5
Amandeep Singh <aman.bamrah25@gmail.com>
2024-11-21 11:59:16 +0530

    added PersonalDetails service.

---
+++ PersonalDetailsService/src/main/java/com/grabbingthecode/personaldetailsservice/PersonalDetailsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.grabbingthecode.personaldetailsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDetailsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalDetailsServiceApplication.class, args);
+    }
+
+}
---
+++ PersonalDetailsService/src/test/java/com/grabbingthecode/personaldetailsservice/PersonalDetailsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grabbingthecode.personaldetailsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalDetailsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
