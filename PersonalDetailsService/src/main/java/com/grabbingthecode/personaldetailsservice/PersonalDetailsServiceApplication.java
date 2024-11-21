a950aa480fbe158f9b9a08e477f9ce544a09d8b3
Amandeep Singh <aman.bamrah25@gmail.com>
2024-11-21 16:42:56 +0530

    updated some files

--- PersonalDetailsService/src/main/java/com/grabbingthecode/personaldetailsservice/PersonalDetailsServiceApplication.java
+++ PersonalDetailsService/src/main/java/com/grabbingthecode/personaldetailsservice/PersonalDetailsServiceApplication.java
@@ -2,8 +2,10 @@ package com.grabbingthecode.personaldetailsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PersonalDetailsServiceApplication {
 
     public static void main(String[] args) {
