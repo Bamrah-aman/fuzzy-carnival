baaa5d69d48942e60dba8cde2386ef2c7b590386
Amandeep Singh <aman.bamrah25@gmail.com>
2024-11-23 11:56:28 +0530

    Established connection to auth service via gateway

---
+++ auth-service/src/main/java/com/grabbingthecode/authservice/demo/DemoController.java
@@ -0,0 +1,15 @@
+package com.grabbingthecode.authservice.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth-demo")
+public class DemoController {
+
+    @GetMapping
+    public String get() {
+        return "GET :: IF YOU ARE SEEING THIS MESSAGE WE HAVE SUCCESSFULLY CONNECTED AUTH SERVICE VIA GATEWAY";
+    }
+}
